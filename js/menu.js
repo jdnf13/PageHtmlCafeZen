d66c43f2f045ef0b1fb73589e7d9e7765349191f
jdnf13 <jdnf13@outlook.es>
2020-11-12 17:31:13 -0500

    pagina cafe zen

---
+++ js/menu.js
@@ -0,0 +1,5 @@
+let btnMenu = document.getElementById("btn-menu");
+let nav =   document.getElementById("nav");
+btnMenu.addEventListener('click', function(){
+    nav.classList.toggle('mostrar');
+});
